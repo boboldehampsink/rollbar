bc03bae48d3f598b341400a70f0fd49fe09d9473
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-05-13 16:30:25 +0200

    Fixes and phpDocs

--- variables/RollbarVariable.php
+++ variables/RollbarVariable.php
@@ -2,22 +2,54 @@
 
 namespace Craft;
 
+/**
+ * Rollbar Variable.
+ *
+ * Lets you use Rollbar in templates
+ *
+ * @author    Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
+ * @copyright Copyright (c) 2015, Bob Olde Hampsink
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ *
+ * @link      http://github.com/boboldehampsink
+ * @since     1.1
+ */
 class RollbarVariable
 {
-    private $settings;
+    /**
+     * Settings.
+     *
+     * @var object
+     */
+    private $_settings;
 
+    /**
+     * Constructor.
+     *
+     * Gets plugin settings for internal use.
+     */
     public function __construct()
     {
-        $this->settings = craft()->plugins->getPlugin('rollbar')->getSettings();
+        $this->_settings = craft()->plugins->getPlugin('rollbar')->getSettings();
     }
 
+    /**
+     * Returns Client Access Token.
+     *
+     * @return string
+     */
     public function clientAccessToken()
     {
-        return $this->settings->getAttribute('clientAccessToken');
+        return $this->_settings->getAttribute('clientAccessToken');
     }
 
+    /**
+     * Returns Reporting in devMode.
+     *
+     * @return string
+     */
     public function reportInDevMode()
     {
-        return $this->settings->getAttribute('reportInDevMode');
+        return $this->_settings->getAttribute('reportInDevMode');
     }
 }
